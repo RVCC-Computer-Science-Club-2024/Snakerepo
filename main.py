35e7fc01467ff5a3cb45facb62fd9f018edfc085
Amodh <amodh1257@gmail.com>
2024-10-17 16:51:58 -0400

    Added explanatory comments

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame, sys
 from pygame.locals import QUIT
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Snake')
+pygame.init() # Initializes pygame module
+display_surface = pygame.display.set_mode((400, 300)) # Sets window size
+pygame.display.set_caption('Snake') # Sets window title
+clock = pygame.time.Clock()
+
 while True:
-   for event in pygame.event.get():
-       if event.type == QUIT:
-           pygame.quit()
-           sys.exit()
-   pygame.display.update()
+   for event in pygame.event.get(): #Gets a list of all events
+       if event.type == QUIT: # Checks if X is clicked
+           pygame.quit() # Closes window
+           sys.exit() # Closes program
+    
+    
+   pygame.display.update() # Updates screen; completes 1 game loop
+   
