8d8eebfced59af500007a3fb6977c7ad74a1fa7f
Amodh <amodh1257@gmail.com>
2024-10-17 23:37:02 -0400

    God am I an idiot. Removed excess code that hoped over from fixing commits.

--- main.py
+++ main.py
@@ -120,16 +120,3 @@ def main():
 
 if __name__ == "__main__":
     main() # Godspeed :saluting_face:
-pygame.init() # Initializes pygame module
-display_surface = pygame.display.set_mode((400, 300)) # Sets window size
-pygame.display.set_caption('Snake') # Sets window title
-clock = pygame.time.Clock()
-
-while True:
-   for event in pygame.event.get(): #Gets a list of all events
-       if event.type == QUIT: # Checks if X is clicked
-           pygame.quit() # Closes window
-           sys.exit() # Closes program
-    
-    
-   pygame.display.update() # Updates screen; completes 1 game loop
