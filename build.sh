30ed831ef1bc77a4d52e80b4a021efdce10b351b
weekOldRoadkill <freddymushroom@gmail.com>
2024-10-31 23:55:57 -0400

    Added venv to scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 DIR=$PWD
 cd $(dirname $0)
-${PYTHON:=python3} -m ensurepip
+${PYTHON:=python3} -m venv venv
+source venv/bin/activate
+${PYTHON} -m ensurepip
 ${PYTHON} -m pip install pipreqs nuitka
 ${PYTHON} -m pipreqs.pipreqs
 ${PYTHON} -m pip install -r requirements.txt
-${PYTHON} -m nuitka --onefile --windows-console-mode=disable main.py
+${PYTHON} -m nuitka --onefile --windows-console-mode=disable --macos-create-app-bundle main.py
 cd $DIR
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 DIR=$PWD
 cd $(dirname $0)
-${PYTHON:=python3} -m ensurepip
+${PYTHON:=python3} -m venv venv
+source venv/bin/activate
+${PYTHON} -m ensurepip
 ${PYTHON} -m pip install pipreqs
 ${PYTHON} -m pipreqs.pipreqs
 ${PYTHON} -m pip install -r requirements.txt
