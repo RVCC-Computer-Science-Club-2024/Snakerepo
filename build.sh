4dfe697539f1fac0c364970406b4455ccfb4e766
weekOldRoadkill <freddymushroom@gmail.com>
2024-10-31 23:19:17 -0400

    Added build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 DIR=$PWD
 cd $(dirname $0)
 ${PYTHON:=python3} -m ensurepip
-${PYTHON} -m pip install pipreqs
+${PYTHON} -m pip install pipreqs nuitka
 ${PYTHON} -m pipreqs.pipreqs
 ${PYTHON} -m pip install -r requirements.txt
-${PYTHON} main.py
+${PYTHON} -m nuitka --onefile --windows-console-mode=disable main.py
 cd $DIR
